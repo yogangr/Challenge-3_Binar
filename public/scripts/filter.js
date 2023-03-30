994813a9fb23e8b537eae07f12b8d7df70a9c145
yogangr <ynugroho487@gmail.com>
2023-03-30 14:35:25 +0700

    update filter

---
+++ public/scripts/filter.js
@@ -0,0 +1,156 @@
+class Car {
+  constructor(cars) {
+    this.cars = cars;
+  }
+  filterCarAvailable() {
+    return this.cars.filter((car) => car.available === true);
+  }
+  filterCarUnAvailable() {
+    return this.cars.filter((car) => car.available === false);
+  }
+
+  filterCarByUser() {
+    let driver = document.getElementById("driver").value;
+    let date = document.getElementById("date").value;
+    let time = document.getElementById("time").value;
+    let dateTime = date + time;
+    let passanger = document.getElementById("passanger").value;
+
+    if (driver === undefined || driver === "") {
+      alert("Please select a driver");
+      return;
+    } else if (dateTime < getDateTimeNow()) {
+      alert("Harap pilih tanggal dan waktu yang lebih dari saat ini");
+      return;
+    } else if (passanger == "" && driver.toString() == "true") {
+      return this.cars.filter(
+        (car) => car.available === true && car.availableAt >= dateTime
+      );
+    } else if (passanger != "" && driver.toString() == "true") {
+      return this.cars.filter(
+        (car) =>
+          car.available === true &&
+          car.capacity >= passanger &&
+          car.availableAt >= dateTime
+      );
+    } else if (passanger == "" && driver.toString() == "false") {
+      return this.cars.filter(
+        (car) => car.available === false && car.availableAt >= dateTime
+      );
+    } else if (passanger != "" && driver.toString() == "false") {
+      return this.cars.filter(
+        (car) =>
+          car.available === false &&
+          car.capacity >= passanger &&
+          car.availableAt >= dateTime
+      );
+    }
+  }
+}
+
+// Module Request
+let xmlHttp = new XMLHttpRequest();
+xmlHttp.open("GET", "http://localhost:8000/api/cars", false);
+xmlHttp.send(null); // Request body null
+
+// Get Data from JSON
+let data = JSON.parse(xmlHttp.responseText);
+
+// Filter Car by Available
+let cars = new Car(data);
+
+// Get Element by ID carsList
+let app = document.getElementById("dataCars");
+htmlData = "";
+
+// Get Data from API
+data = cars.filterCarAvailable();
+data = cars.filterCarUnAvailable();
+
+// Function Format Rupiah
+function rupiah(number) {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(number);
+}
+
+// Trigger Function by Button id=btnFilterCar
+let btnFilterCar = document
+  .getElementById("btnFilter")
+  .addEventListener("click", getCars);
+
+// Function Number Format Rupiah
+function getDateTimeNow() {
+  let today = new Date();
+  let date =
+    today.getFullYear() +
+    "-" +
+    String(today.getMonth() + 1).padStart(2, "0") +
+    "-" +
+    String(today.getDate()).padStart(2, "0");
+  let time =
+    String(today.getHours()).padStart(2, "0") +
+    ":" +
+    String(today.getMinutes()).padStart(2, "0") +
+    ":" +
+    String(today.getSeconds()).padStart(2, "0");
+  let dateNow = date + "T" + time + ".000Z";
+  return dateNow;
+}
+
+// Loop Data
+function getCars() {
+  let htmlData = "";
+  data = cars.filterCarByUser();
+  if (data === "" || data === undefined) {
+    htmlData = "";
+    app.innerHTML = htmlData;
+    return;
+  } else {
+    for (let index = 0; index < data.length; index++) {
+      let car = data[index];
+      let rentCost = rupiah(car.rentPerDay);
+      htmlData += `<div style="width: 333px; box-shadow: 0px 0px 4px rgba(0, 0, 0, 0.15); border-radius: 8px; padding: 20px; height: fit-content; margin-bottom: 35px;">
+                      <img src=${car.image} alt="${car.manufacture}" style="width: 100%; height: 222px; border-radius: 3px;">
+                      <p style="margin: 16px 0px 8px 0px; font-family: 'Helvetica'; font-style: normal; font-weight: 400;font-size: 14px; line-height: 20px;">${car.manufacture} ${car.model}</p>
+                      <h5 style="margin-bottom: 8px; font-family: 'Helvetica'; font-style: normal; font-weight: 700; font-size: 16px; line-height: 24px;">${rentCost} / hari</h5>
+                      <h6 style="height: 60px; margin-bottom: 16px; font-family: 'Helvetica'; font-style: normal; font-weight: 300; font-size: 14px; line-height: 20px;">${car.description}</h6>
+                      <div style="display: flex; ">
+                          <div style="margin-right: 8px; padding: 0px;"> 
+                              <i class="bi bi-people" aria-hidden="true" style="font-size:24px;"></i>
+                          </div> 
+                          <p style="font-family: 'Helvetica'; font-style: normal; font-weight: 300; font-size: 14px; line-height: 26px;">
+                          ${car.capacity} Orang
+                          </p>
+                      </div>
+                      <div style="display: flex; ">
+                          <div style="margin-right: 12px; padding: 2px 0px;"> 
+                              <i class="bi bi-gear" aria-hidden="true" style="font-size:20px;"></i>
+                          </div> 
+                          <p style="font-family: 'Helvetica'; font-style: normal; font-weight: 300; font-size: 14px; line-height: 26px;">
+                          ${car.transmission}
+                          </p>
+                      </div>
+                      <div style="display: flex;">
+                          <div style="margin-right: 12px; padding: 2px 0px;"> 
+                              <i class="bi bi-calendar4" aria-hidden="true" style="font-size:20px;"></i>
+                          </div> 
+                          <p style="font-family: 'Helvetica'; font-style: normal; font-weight: 300; font-size: 14px; line-height: 26px;">
+                          ${car.year}
+                          </p>
+                      </div>
+                      <button style="margin-top:8px; width: 100%; padding: 14px 0px; background: #5CB85F; border-radius: 3px; border: none; color: white; text-align: center; text-decoration: none; font-family: 'Helvetica';
+                      font-style: normal;
+                      font-weight: 700;
+                      font-size: 14px; font-family: 'Helvetica'; line-height: 20px;">
+                          Pilih Mobil
+                      </button>
+                  </div>`;
+    }
+    app.innerHTML = htmlData;
+    if (htmlData == "") {
+      alert("No car available");
+    }
+  }
+}
--- server/index.js
+++ server/index.js
@@ -1,27 +1,68 @@
 const http = require("http");
+const { PORT = 8000 } = process.env; // Ambil port dari environment variable
+
 const fs = require("fs");
+const path = require("path");
+// const mime = require("mime");
 
-const port = 8000;
+const cars = require("../data/cars.min.json");
 
-http
-  .createServer((req, res) => {
-    switch (req.url) {
-      case "/":
-        req.url = "index.html";
-        break;
-      case "/cars":
-        req.url = "search-car.html";
-        break;
-    }
-    let path = "public/" + req.url;
-    fs.readFile(path, (err, data) => {
-      res.writeHead(200);
-      res.end(data);
+// Request handler
+// Fungsi yang berjalan ketika ada request yang masuk.
+function onRequest(req, res) {
+  if (req.url === "/") {
+    fs.readFile("./public/index.html", "UTF-8", function (err, html) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(html);
     });
-  })
-  .listen(port, "localhost", () => {
-    console.log(
-      "Server sudah berjalan, silahkan buka http://localhost:%d",
-      port
-    );
-  });
+  } else if (req.url === "/cars") {
+    fs.readFile("./public/search-car.html", "UTF-8", function (err, html) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(html);
+    });
+  } else if (req.url === "/getcars") {
+    const dataPath = path.join(__dirname, "../data", "/cars.json");
+    const fileStream = fs.createReadStream(dataPath, "UTF-8");
+    res.writeHead(200, { "Content-Type": "application/json" });
+    fileStream.pipe(res);
+  } else if (req.url.match(".css$")) {
+    //  console.log('masuk sini====css===');
+    const cssPath = path.join(__dirname, "../public", req.url);
+    const fileStream = fs.createReadStream(cssPath, "UTF-8");
+    res.writeHead(200, { "Content-Type": "text/css" });
+    fileStream.pipe(res);
+  } else if (req.url.match(".png$")) {
+    const imagePath = path.join(__dirname, "../public", req.url);
+    const fileStream = fs.createReadStream(imagePath);
+    res.writeHead(200, { "Content-Type": "image/png" });
+    fileStream.pipe(res);
+  } else if (req.url.match(".jpg$")) {
+    const imagePath = path.join(__dirname, "../public", req.url);
+    const fileStream = fs.createReadStream(imagePath);
+    res.writeHead(200, { "Content-Type": "image/jpg" });
+    fileStream.pipe(res);
+  } else if (req.url.match(".js$")) {
+    //  console.log('masuk sini===js====');
+    const jsPath = path.join(__dirname, "../public", req.url);
+    const fileStream = fs.createReadStream(jsPath);
+    res.writeHead(200, { "Content-Type": "application/javascript" });
+    fileStream.pipe(res);
+  } else if (req.url === "/api/cars") {
+    res.setHeader("Content-Type", "application/json");
+    res.writeHead(200);
+    res.end(JSON.stringify(cars));
+    return;
+  } else {
+    fs.readFile("./public/404_handling.html", "UTF-8", function (err, html) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end(html);
+    });
+  }
+}
+
+const server = http.createServer(onRequest);
+
+// Jalankan server
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server sudah berjalan, silahkan buka http://localhost:%d", PORT);
+});
